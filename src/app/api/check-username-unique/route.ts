b9dc9b252f890c81691317ff4f4f61897dcb09de
amanyall <amanpandey5961@gmail.com>
2024-12-15 22:59:22 +0530

    route

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -28,8 +28,21 @@ export async function GET(request:Request) {
           }, {status: 400})
         }
 
-        
+        const {username} = result.data
 
+        const existingVerifiedUser = await UserModel.findOne({username, isVerified:true})
+
+        if (existingVerifiedUser) {
+            return Response.json({
+                success: false,
+                message:'Username is already takekn',
+              }, {status: 400})
+        }
+
+        return Response.json({
+            success: true,
+            message:'Username is unique',
+          }, {status: 400})
 
     } catch (error) {
         console.error("Error checking username", error)
