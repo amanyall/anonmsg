dd703f215adef4a196a0c962c1986147908edf06
amanyall <amanpandey5961@gmail.com>
2025-01-02 23:20:35 +0530

    OTP verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
+
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +31,7 @@ export default function RootLayout({
       <AuthProvider>
       <body>
         {children}
+        <Toaster/>
       </body>
       </AuthProvider>
       
